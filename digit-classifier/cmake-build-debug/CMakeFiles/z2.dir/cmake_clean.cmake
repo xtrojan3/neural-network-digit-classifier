d34f06ac821c0f5d0319f2503923e1469ea71926
33matusk <matustrojan@gmail.com>
2026-06-15 19:37:42 +0200

    rename project folder

---
+++ digit-classifier/cmake-build-debug/CMakeFiles/z2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/z2.dir/src/data.c.obj"
+  "CMakeFiles/z2.dir/src/data.c.obj.d"
+  "CMakeFiles/z2.dir/src/functions.c.obj"
+  "CMakeFiles/z2.dir/src/functions.c.obj.d"
+  "CMakeFiles/z2.dir/src/z2.c.obj"
+  "CMakeFiles/z2.dir/src/z2.c.obj.d"
+  "libz2.dll.a"
+  "z2.exe"
+  "z2.exe.manifest"
+  "z2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/z2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
